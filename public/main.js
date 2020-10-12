bfebd85475b0e89d423135eb2fa18a8174a04157
rootz491 <experimentyt1@gmail.com>
2020-10-13 00:36:53 +0530

    UI ready

---
+++ public/main.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDu_-C_KPUM7Dykwhodkzz64XpzVWhpXB0",
+    authDomain: "connect-491.firebaseapp.com",
+    databaseURL: "https://connect-491.firebaseio.com",
+    projectId: "connect-491",
+    storageBucket: "connect-491.appspot.com",
+    messagingSenderId: "81938691198",
+    appId: "1:81938691198:web:1e501702444763e3ae142f",
+    measurementId: "G-KFW850R2XC"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
